a3b8d441428e821286a44dd8cd501e761688df1e
Daniel Mouimi <mouimidaniel@gmail.com>
2026-05-13 18:42:43 +0200

    update database with migrations

--- CRM.Api/obj/Debug/net10.0/CRM.Api.AssemblyInfo.cs
+++ CRM.Api/obj/Debug/net10.0/CRM.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6baf6c1c4bcdf73f6aa041f59c8fc7fb54a9f81b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc6349764b08bc73d3ea62d6594763c5552ee0d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
